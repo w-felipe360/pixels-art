a6f4ba986fa2b10417b38f083a9155b0c6690a0b
Willian Felipe <w.felipebraz@gmail.com>
2022-03-23 21:43:59 -0300

    terminei gracas a deus

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let cores = document.getElementsByClassName('color')
 let preto = document.getElementById("black");
+
 function quadrados() {
 
     let pai = document.getElementById('pixel-board');
@@ -8,19 +9,20 @@ function quadrados() {
         let elementos = document.createElement('div');
         elementos.className = 'pixel'
         pai.appendChild(elementos);
+
     }
 }
 quadrados();
-
+let cubos = document.getElementsByClassName('pixel')
 
 
 window.onload
 
 preto.classList.add('selected');
 
-for(let index = 0; index < cores.length; index++)
+for(let index = 0; index < cores.length; index++){
 
-cores[index].addEventListener('click', selecionaCor);
+cores[index].addEventListener('click', selecionaCor);}
 function selecionaCor(event) {
     
     for (let index = 0; index < cores.length; index += 1) {
@@ -33,3 +35,34 @@ function selecionaCor(event) {
     }
 event.target.classList.add('selected')
 }
+for(let index2 = 0; index2 < cubos.length; index2++)
+cubos[index2].addEventListener('click', colocaCor)
+function colocaCor(event){
+    if (document.getElementById("black").className.includes('selected')){
+        event.target.classList.add('blackme') 
+        event.target.classList.remove('blueme') 
+        event.target.classList.remove('redme') 
+        event.target.classList.remove('greenme') 
+    } else if (document.getElementById("green").className.includes('selected')){
+        event.target.classList.add('greenme')
+        event.target.classList.remove('redme') 
+        event.target.classList.remove('blackme') 
+        event.target.classList.remove('blueme') 
+    }
+        else if (document.getElementById("red").className.includes('selected')){
+            event.target.classList.add('redme')
+            event.target.classList.remove('blueme') 
+            event.target.classList.remove('greenme') 
+            event.target.classList.remove('blackme') 
+        }
+            else if (document.getElementById("blue").className.includes('selected')){
+                event.target.classList.add('blueme')
+        event.target.classList.remove('redme') 
+        event.target.classList.remove('blackme') 
+        event.target.classList.remove('greenme') 
+    }
+        
+    
+
+}
+
